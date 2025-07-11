2be4eeabdb14b787c230c4c84d0641fed1f8232f
imanikmd <nurimani3002@gmail.com>
2025-07-11 22:26:21 +0800

    feat: Enhance MyButton tap handling and improve login page layout with Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
-
-import 'package:anogi_delivery/firebase_options.dart';
-import 'package:anogi_delivery/models/restaurant.dart';
-import 'package:anogi_delivery/services/auth_gate.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+
+import 'firebase_options.dart';
+import 'models/restaurant.dart';
+import 'services/auth_gate.dart';
 import 'themes/theme_provider.dart';
+import 'pages/home_page.dart';        
+import 'pages/login_page.dart';     
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
-    MultiProvider(providers: [
-      ChangeNotifierProvider(create: (context) => ThemeProvider()),
-      ChangeNotifierProvider(create: (_) => Restaurant()), 
-      // restaurant provider
-      // ChangeNotifierProvider(create: (context) => Restaurant()),
-    ],
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+        ChangeNotifierProvider(create: (_) => Restaurant()),
+      ],
       child: const MyApp(),
     ),
   );
@@ -24,12 +28,23 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const AuthGate(),
+      title: 'Anogi Delivery',
       theme: Provider.of<ThemeProvider>(context).themeData,
+
+
+      routes: {
+        '/home': (context) => const HomePage(),
+        '/login': (context) => LoginPage(onTap: () {
+          Navigator.pushNamed(context, '/register');
+        }),
+      },
+
+      home: const AuthGate(),
     );
   }
 }
